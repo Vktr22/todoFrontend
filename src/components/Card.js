24dad8369a06aba228197ed7fde6f176f1a82342
Nádas Viktória Mária (SZF_2024_2026) <nadas.viktoria.maria@diak.szamalk-szalezi.hu>
2026-04-22 16:37:18 +0200

    ..

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+
+
+
+export default function Card() {
+    return (
+        <div className="tevekenysegKartya">
+           bgfbgf
+        </div>
+    );
+}
